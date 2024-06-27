d10a797e19cf4a9391cb896de50f4d01945bd957
suhasbbhat <162411870+suhasbbhat@users.noreply.github.com>
2024-06-27 16:40:11 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -3,18 +3,15 @@
 
 #include <string>
 #include <vector>
-#include <utility>
 
 class StringCalculator {
 public:
-    int add(const std::string& input);
-    std::pair<std::string, std::string> ExtractDelimiter(const std::string& numbers);
-    std::string ReplaceNewlineWithComma(const std::string& input);
-    std::vector<int> Tokenize(const std::string& input, const std::string& delimiter);
-    int SumNumbers(const std::vector<int>& numbers);
+    int Add(const std::string& input);
 
 private:
-    std::vector<int> FilterNumbers(const std::vector<int>& numbers);
+    std::vector<int> Tokenize(const std::string& input);
+    int SumNumbers(const std::vector<int>& numbers) const;
+    void ValidateNumber(int number) const;
 };
 
 #endif // STRINGCALCULATOR_H
