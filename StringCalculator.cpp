6ba0bd75bddfdfd658ea81527e58fdff0b1e59ff
suhasbbhat <162411870+suhasbbhat@users.noreply.github.com>
2024-06-27 16:10:03 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -38,3 +38,14 @@ int StringCalculator::add(std::string numbers) {
     std::replace(numbers.begin(), numbers.end(), '\n', ',');
     return SumNumbers(numbers, delimiter);
 }
+
+std::vector<std::string> StringCalculator::Tokenize(const std::string& input, const std::string& delimiter) {
+    std::vector<std::string> tokens;
+    size_t start = 0, end = 0;
+    while ((end = input.find(delimiter, start)) != std::string::npos) {
+        tokens.push_back(input.substr(start, end - start));
+        start = end + delimiter.length();
+    }
+    tokens.push_back(input.substr(start));
+    return tokens;
+}
