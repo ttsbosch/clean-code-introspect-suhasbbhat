c7f14929115b58a046b66b1bbed912c7dfd31336
suhasbbhat <162411870+suhasbbhat@users.noreply.github.com>
2024-06-27 16:24:12 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,6 +1,5 @@
 #include "StringCalculator.h"
 #include <sstream>
-#include <stdexcept>
 #include <algorithm>
 
 int StringCalculator::add(const std::string& input) {
@@ -8,40 +7,18 @@ int StringCalculator::add(const std::string& input) {
         return 0;
     }
 
-    auto [delimiter, numbers] = ExtractDelimiter(input);
+    // Refactored to avoid structured bindings
+    auto delimiterAndNumbers = ExtractDelimiter(input);
+    std::string delimiter = delimiterAndNumbers.first;
+    std::string numbers = delimiterAndNumbers.second;
+
     std::string modifiedNumbers = ReplaceNewlineWithComma(numbers);
     std::vector<int> tokens = Tokenize(modifiedNumbers, delimiter);
-    std::vector<int> filteredTokens = FilterNumbers(tokens);
+    std::vector<int> filteredTokens = FilterNumbers(tokens); // Ensure this is correctly called
     return SumNumbers(filteredTokens);
 }
 
-std::pair<std::string, std::string> StringCalculator::ExtractDelimiter(const std::string& numbers) {
-    if (numbers.substr(0, 2) == "//") {
-        size_t delimiterEnd = numbers.find("\n");
-        std::string delimiter = numbers.substr(2, delimiterEnd - 2);
-        std::string modifiedNumbers = numbers.substr(delimiterEnd + 1);
-        return {delimiter, modifiedNumbers};
-    }
-    return {",", numbers};
-}
-
-std::string StringCalculator::ReplaceNewlineWithComma(const std::string& input) {
-    std::string result = input;
-    std::replace(result.begin(), result.end(), '\n', ',');
-    return result;
-}
-
-std::vector<int> StringCalculator::Tokenize(const std::string& input, const std::string& delimiter) {
-    std::vector<int> numbers;
-    size_t start = 0, end = 0;
-    while ((end = input.find(delimiter, start)) != std::string::npos) {
-        int num = std::stoi(input.substr(start, end - start));
-        numbers.push_back(num);
-        start = end + delimiter.length();
-    }
-    numbers.push_back(std::stoi(input.substr(start)));
-    return numbers;
-}
+// Ensure other methods are implemented as previously described
 
 std::vector<int> StringCalculator::FilterNumbers(const std::vector<int>& numbers) {
     std::vector<int> filteredNumbers;
@@ -51,12 +28,4 @@ std::vector<int> StringCalculator::FilterNumbers(const std::vector<int>& numbers
         }
     }
     return filteredNumbers;
-}
-
-int StringCalculator::SumNumbers(const std::vector<int>& numbers) {
-    int sum = 0;
-    for (int num : numbers) {
-        sum += num;
-    }
-    return sum;
 }
