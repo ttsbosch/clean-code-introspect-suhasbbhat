55bb4b48cb9fe5f78834fe8f1b60f6e9e407f14e
suhasbbhat <162411870+suhasbbhat@users.noreply.github.com>
2024-06-27 15:50:23 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,10 +2,15 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
+#include <vector>
 
 class StringCalculator {
 public:
-    int add(const std::string& input);
+    int Add(const std::string& numbers);
+
+private:
+    std::vector<std::string> Tokenize(const std::string& input, const std::string& delimiters);
+    void ValidateNegatives(const std::vector<int>& numbers);
 };
 
 #endif // STRINGCALCULATOR_H
