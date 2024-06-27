a68307243e3d7d4df32b52e2f25468d1931d526d
suhasbbhat <162411870+suhasbbhat@users.noreply.github.com>
2024-06-27 16:44:26 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -17,17 +17,29 @@ std::vector<int> StringCalculator::Tokenize(const std::string& input) {
     std::string token;
 
     while (std::getline(stream, token, ',')) {
-        std::replace(token.begin(), token.end(), '\n', ',');
-        std::istringstream tokenStream(token);
-        std::string number;
-        while (std::getline(tokenStream, number, ',')) {
-            int num = std::stoi(number);
-            ValidateNumber(num);
-            if (num <= 1000) tokens.push_back(num);
-        }
+        ProcessToken(token, tokens);
     }
 
     return tokens;
+}
+
+void StringCalculator::ProcessToken(std::string& token, std::vector<int>& tokens) {
+    ReplaceNewlinesWithCommas(token);
+    std::istringstream tokenStream(token);
+    std::string number;
+    while (std::getline(tokenStream, number, ',')) {
+        ProcessNumber(number, tokens);
+    }
+}
+
+void StringCalculator::ReplaceNewlinesWithCommas(std::string& token) {
+    std::replace(token.begin(), token.end(), '\n', ',');
+}
+
+void StringCalculator::ProcessNumber(const std::string& number, std::vector<int>& tokens) {
+    int num = std::stoi(number);
+    ValidateNumber(num);
+    if (num <= 1000) tokens.push_back(num);
 }
 
 int StringCalculator::SumNumbers(const std::vector<int>& numbers) const {
